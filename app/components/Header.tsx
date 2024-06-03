cc44dec8ef026932a1c2a7272d907560f4da931e
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-06-03 22:57:23 +0100

    stash

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between items-center h-[28rem] mt-10 ">
-      <section className="w-[50%] relative  px-24 py-24 flex flex-col gap-4">
+    <header
+      className="flex flex-row justify-between items-center h-[28rem] px-40 py-4 mt-10 bg-center bg-cover"
+      style={{ backgroundImage: "url('/patterns/pattern-4.svg')" }}
+    >
+      <section className="w-[50%] relative pb-10   flex flex-col gap-4 ">
         <div className="flex flex-col gap-2">
           <h5 className=" font-medium text-lg">Hi, I'm</h5>
           <h1 className=" font-bold text-6xl">Farouk Mustapha</h1>
@@ -16,16 +19,20 @@ const Header = () => {
           </p>
         </div>
       </section>
-      <section
-        className=" flex justify-center items-center w-[50%] bg-cover bg-center h-full"
-        style={{ backgroundImage: "url('/patterns/pattern-4.svg')" }}
-      >
+      <section className=" flex justify-center items-center w-[50%] h-full">
         <Image
           alt="profile image"
-          src={"/images/profile.jpg"}
-          style={{ borderRadius: "10%" }}
+          src={"/images/profile-7.jpg"}
+          style={{
+            borderRadius: "10%",
+            clipPath:
+              "polygon(50% 0, 100% 20%, 100% 80%, 50% 100%, 0 80%, 0 20%)",
+            boxShadow:
+              "0 4px 15px rgba(255, 225, 204, 0.7), 0 4px 15px rgba(254, 197, 157, 0.5), 0 4px 15px rgba(255, 139, 61, 0.3)",
+            filter: "brightness(0.9) contrast(1.1)",
+          }}
           objectFit="cover"
-          height={"100"}
+          height={"150"}
           width={"300"}
         />
       </section>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <section className="relative flex flex-row w-full">
       <div className=" w-[60%]">
-        <nav className=" w-full flex flex-row justify-between items-center border-r-2 border-b-2 border-r-secondary-color-2 border-b-secondary-color-2 border-solid py-6 px-24  ">
+        <nav className=" w-full flex flex-row justify-between items-center border-r-2 border-b-2 border-r-secondary-color-2 border-b-secondary-color-2 border-solid py-6 px-40">
           <div>
             <h1>Dev Farouk</h1>
           </div>
