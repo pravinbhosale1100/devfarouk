c25eb2cb60cd65643fe61c292a0e917c0ee75f86
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-09-15 12:42:53 +0100

    spelling error

--- app/sections/Copyright.tsx
+++ app/sections/Copyright.tsx
@@ -14,7 +14,7 @@ const Copyright = () => {
           {" "}
           devfarouk 👨‍💻
         </Link>{" "}
-        (Inpsirations from{" "}
+        (Inspirations from{" "}
         <Link
           href={"https://www.geekmaros.dev/"}
           className=" font-bold"
